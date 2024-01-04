0d65fba194ac013b2b4d0d7ac2faa88684bf98e1
Calderoni <marcelo@transvectologistics.com>
2024-01-04 10:39:44 -0600

    server and packages

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// Dependencies
+const { OpenAI } = require('langchain/llms/openai');
+const inquirer = require('inquirer');
+require('dotenv').config();
+
+
+// Creates and stores wrapper for the OpenAI package along with basic configuration
+const model = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0,
+    model: 'gpt-3.5-turbo',
+});
+
+
+// Calling the model
+const promptFunc = async (input) => {
+    try {
+        const res = await model.call(input);
+        console.log(res);
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
+// Initialization function that uses inquirer to prompt the user and returns a promise. It takes the user input and passes it through the call method
+const init = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Ask a coding question:'
+        },
+    ]).then((inquirerResponse) => {
+        promptFunc(inquirerResponse.name);
+    });
+};
+
+// Calls the initialization function and starts the script
+init();
+
+
