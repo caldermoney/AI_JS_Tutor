89b59c4791f503902bcd552e20a1a3fca366a654
Calderoni <marcelo@transvectologistics.com>
2024-01-18 16:06:59 -0600

    working project

--- script.js
+++ script.js
@@ -15,25 +15,36 @@ const model = new OpenAI({
 });
 
 
+
 // Calling the model
 const promptFunc = async (input) => {
     try {
-        // Instantiation of a new object called "prompt" using the "PromptTemplate" class
+        // Structures the output from the model, parses it, and returns it to the user.
+        const parser = StructuredOutputParser.fromNamesAndDescriptions({
+            code: "Javascript code that answers the users question",
+            explanation: "Detailed explanation of the example code provided",
+        });
+        // Parses the output from the model.
+        const formatInstructions = parser.getFormatInstructions();
+
         const prompt = new PromptTemplate({
-            template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{question}",
-            inputVariables: ['question'],
+            template: "You are a JavaScript expert. Respond to the user's coding questions as thorougly as possible in the form of one JSON object. The first key-value pair will be sample code you provide with a key called code and a string value, and the second will be an explanation of the code with a key called explanation and a string value. Use line breaks where appropriate. You must format your output as a JSON value that adheres to a given \"JSON Schema\" instance. Properly use escape characters parsewith a backslash in the response where appropriate to maintain a proper string. Limit the entire response to 250 characters.\n{question}",
+            inputVariables: ["question"],
+            partialVariables: { format_instructions: formatInstructions }
         });
-        // Formats the input and calls the model
+        
         const promptInput = await prompt.format({
-            question: input,
+            question: input
         });
-        const res = await model.call(input);
-        console.log(res);
+        const res = await model.call(promptInput);
+        console.log(await parser.parse(res));
     }
     catch (err) {
-        console.log(err);
+        console.error(err);
     }
 };
+
+
 
 // Initialization function that uses inquirer to prompt the user and returns a promise. It takes the user input and passes it through the call method
 const init = () => {
